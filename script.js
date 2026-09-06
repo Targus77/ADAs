e13a569cff7c4fa3fe7d9ee7db7a6df32f7c9b2b
Targus77 <andres.guerrero20@prepasoficiales.net>
2026-09-05 19:07:55 -0600

    Add countdown timer functionality to script.js

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ function votar(opcion) {
 
     document.getElementById("votosProyector").textContent = votosProyector;
 }
+const fechaFinal = new Date();
+
+fechaFinal.setDate(fechaFinal.getDate() + 15);
+
+
+function actualizarContador() {
+
+    const ahora = new Date();
+
+    const diferencia = fechaFinal - ahora;
+
+
+    const dias = Math.floor(
+        diferencia / (1000 * 60 * 60 * 24)
+    );
+
+
+    const horas = Math.floor(
+        (diferencia / (1000 * 60 * 60)) % 24
+    );
+
+
+    const minutos = Math.floor(
+        (diferencia / (1000 * 60)) % 60
+    );
+
+
+    const segundos = Math.floor(
+        (diferencia / 1000) % 60
+    );
+
+
+    document.getElementById("tiempo").textContent =
+        dias + " días " +
+        horas + "h " +
+        minutos + "m " +
+        segundos + "s";
+}
+
+
+actualizarContador();
+
+
+setInterval(actualizarContador, 1000);
